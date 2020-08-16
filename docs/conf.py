053f02bf2adb344af7b841635e8ea3fba48b4b4f
mercuryconsortium <togo.odbadrakh@furman.edu>
2020-08-15 20:48:37 -0400

    theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,6 @@ release = '0.1.1'
 
 extensions = ['recommonmark','sphinxcontrib.fulltoc']
 templates_path = ['_templates']
-html_theme = 'sphinxdoc'
+html_theme = 'classic'
 html_css_files = ['style.css']
 html_static_path = ['_static']
